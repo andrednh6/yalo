123af65455beceb16f40d4ef4414bca8b623277f
andrednh6 <andred.navarroh@gmail.com>
2021-02-16 09:17:24 -0500

    se agregaron las preguntas y los resultados del test

--- scripts/scrip.js
+++ scripts/scrip.js
@@ -1,4 +1,96 @@
 /* const contenedor = document.getElementById('test'); */
+//Una vez que mande el siguiente se debe activar el siguiente form y asi dos veces mas,
+//recien al enviar deberiamos poder ver el resultado general
+
+
+
+function verificarRespuestas() {
+  const total = 40;
+  let puntosTelescopica = 0;
+  let puntosGranAngular = 0;
+
+  let puntosDependiente = 0;
+  let puntosColaborador = 0;
+  let puntosAutonomo = 0;
+
+  let puntosVisual = 0;
+  let puntosAuditiva = 0;
+  let puntosCinestetica = 0;
+
+  let puntosGlobal = 0;
+  let puntosAnalitica = 0;
+  
+  const myForm = document.forms["quizForm"];
+  //prestar atencion
+  const telescopica = ["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"]
+  const granAngular = ["b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b"]
+  //traducir
+  const dependiente = ["c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c"]
+  const colaborador = ["d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d"]
+  const autonomo =    ["e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e","e"]
+  //relacionar
+  const visual =      ["f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f","f"]
+  const auditiva =    ["g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g"]
+  const cinestetica = ["h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h","h"]
+  //comprender
+  const global =      ["i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i","i"]
+  const analitica =   ["j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j","j"]
+ 
+  for(let i = 1; i <= total; i++){
+    if (myForm["p"+ i].value === null || myForm["p" + i].value === "") {
+      alert("Por favor responde la pregunta " + i);
+      return false;
+    } else {
+      switch (myForm["p"+ i].value) {
+         case telescopica[i - 1]:
+           puntosTelescopica++;
+           break;
+         case granAngular[i - 1]:
+           puntosGranAngular++;
+           break;
+         case dependiente[i - 1]:
+           puntosDependiente++;
+           break;
+         case colaborador[i - 1]:
+           puntosColaborador++;
+           break;
+         case autonomo[i - 1]:
+           puntosAutonomo++;
+           break;
+         case visual[i - 1]:
+           puntosVisual++;
+           break;
+         case auditiva[i - 1]:
+           puntosAuditiva++;
+           break; 
+         case cinestetica[i - 1]:
+           puntosCinestetica++;
+           break; 
+         case global[i - 1]:
+           puntosGlobal++;
+           break;
+        case analitica[i - 1]:
+           puntosAnalitica++;
+      }
+    }
+  }
+
+       // if(myForm["p" + i].value === telescopica[i - 1]){
+       //   puntosTelescopica++;
+       // } else {
+       //   if(myForm["p" + i].value === granAngular[i - 1]){
+       //     puntosGranAngular++;
+       //   }
+       // }
+  var resultado = document.getElementById("resultado");
+  resultado.innerHTML = '<h3>Podemos representar el aprendizaje y comprensión en 4 procesos secuenciales:  <br/> <span> 1. Cómo presta atención a la nueva información o aprendizaje.<br/> 2. Cómo traduce lo que ve, oye y siente, y le encuentra sentido.<br/> 3. Cómo relaciona lo presentado con lo que ya sabe.<br/> 4. Cómo comprende o sintetiza la información, y la extrapola para utilizarla de inmediato o en el futuro.<br/><br/>Prestar atención (motivación para aprender)<br/>' + puntosTelescopica + '/8 --> Telescopica<br/>' + puntosGranAngular + '/8 --> Gran angular <br/><br/>Traducir (Dependencia - independencia en el aprendizaje)<br/>' + puntosDependiente + '/8 --> Dependiente <br/>' + puntosColaborador + '/8 --> Colaborador <br/>' + puntosAutonomo + '/8 --> Autonomo <br/><br/>Relacionar (Percepción de los datos)<br/>' + puntosVisual + '/8 --> Visual <br/>' + puntosAuditiva + '/8 --> Auditiva <br/>' + puntosCinestetica + '/8 --> Cinestética <br/><br/>Comprender (Síntesis de la información)<br/>' + puntosGlobal + '/8 --> Global <br/>' + puntosAnalitica + '/8 --> Analítica </h3>';
+  return false
+}
+  
+ 
+
+
+/*
 
 const preguntas = [
   "1 Prestar Atención",
@@ -16,7 +108,7 @@ const preguntas = [
 
   document.getElementById('test').innerHTML = preguntas[indiceAleatorio];
 
- /* {
+  {
     pregunta: "1 Prestar Atención",
     respuestas: {
       b:"No me gusta participar en seminarios o cursos de formación, a menos que el tema realmente me interese.",
